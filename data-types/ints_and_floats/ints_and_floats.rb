ea1379ea3fd803df4dd932d934a996cbe2ed2a41
Daniel Selph <daniel.p.selph@gmail.com>
2020-07-28 10:26:40 -0600

    Complete ints and floats exercise, all tests passing

--- data-types/ints_and_floats/ints_and_floats.rb
+++ data-types/ints_and_floats/ints_and_floats.rb
@@ -1,85 +1,253 @@
 require 'minitest/autorun'
 require 'minitest/pride'
 
-class IntsAndFloatsTests < Minitest::Test
+# There's a video walk-through of this exercise available here:
+# https://youtu.be/aeAkLxr5diE
+# and a helpful blog post on this general topic here:
+# https://josh.works/turing-backend-prep-02-first-tests-and-making-them-pass
+
+class StringTest < Minitest::Test
   def test_1
-    lucky = 7
-    unlucky = 13
-    # Using the two variables defined above,
-    # add the lucky number and the unlucky number
-    sum = lucky + unlucky
-    assert_equal 20, sum
+    # skip
+    name = "alice"
+    # In place of the line below, call a method on the name variable
+    # defined above to acheive the expected output.
+    actual = name.capitalize
+    expected = "Alice"
+
+    assert_equal expected, actual
   end
 
   def test_2
-    lucky = 7
-    unlucky = 13
-    # Using the two variables defined above,
-    # subtract the unlucky from the lucky
-    difference = lucky - unlucky
-    assert_equal -6, difference
+    # skip
+    name = "aLiCe"
+    # In place of the line below, call a method to achieve the expected output.
+    actual = name.upcase
+    expected = "ALICE"
+
+    assert_equal expected, actual
   end
 
   def test_3
-    lucky = 7
-    unlucky = 13
-    # Using the two variables defined above,
-    # divide unlucky by lucky
-    # NOTE: this is integer division
-    quotient = unlucky / lucky
-    assert_equal 1, quotient
+    # skip
+    name = "AlIcE"
+    # In place of the line below, call a method to achieve the expected output.
+    actual = name.downcase
+    expected = "alice"
+
+    assert_equal expected, actual
   end
 
   def test_4
-    lucky = 7
-    unlucky = 13
-    # Using the two variables defined above,
-    # divide unlucky by lucky
-    quotient = unlucky.fdiv(lucky)
-    assert_equal 1.8571428571428572, quotient
+    # skip
+    rhyme = "peter piper picked a peck of picked peppers"
+    # In place of the line below, call a method to achieve the expected output.
+    actual = rhyme.reverse
+    expected = "sreppep dekcip fo kcep a dekcip repip retep"
+
+    assert_equal expected, actual
   end
 
   def test_5
-    lucky = 7
-    unlucky = 13
-    # Using the two variables defined above,
-    # find the remainder of the unlucky divided by the lucky
-    remainder = unlucky.modulo(lucky)
-    assert_equal 6, remainder
+    # skip
+    word = "ticking"
+    # In place of the line below, call a method to achieve the expected output.
+    actual = word.gsub("t", "k")
+    expected = "kicking"
+
+    assert_equal expected, actual
   end
 
   def test_6
-    lucky = 7
-    # Using the variable defined above,
-    # find out if the lucky number is even
-    even = lucky.even?
-    assert_equal false, even
+    # skip
+    word = "ticking"
+    # In place of the line below, call a method to achieve the expected output.
+    actual = word.gsub('ti', 'clo')
+    expected = "clocking"
+
+    assert_equal expected, actual
   end
 
   def test_7
-    pi = 3.14
-    # Using the variable defined above,
-    # round the number to the nearest whole number
-    rounded = pi.round(0)
-    assert_equal 3, rounded
+    # skip
+    words = "five sleepy kittens"
+    # In place of the line below, call a method to achieve the expected output.
+    actual = words.gsub('e', '*')
+    expected = "fiv* sl**py kitt*ns"
+
+    assert_equal expected, actual
   end
 
   def test_8
-    pi = 3.14
-    # Using the variable defined above,
-    # round the number to one decimal place
-    rounded = pi.round(1)
-    assert_equal 3.1, rounded
+    # skip
+    greeting = "Hello!!"
+    # In place of the line below, call a method to achieve the expected output.
+    actual = greeting.chop
+    expected = "Hello!"
+
+    assert_equal expected, actual
   end
 
-
-
   def test_9
-    skip
-    pi = 3.14
-    # Using the variable defined above,
-    # round the number to the next highest whole number
-    rounded = _________
-    assert_equal 4, rounded
+    # skip
+    greeting = "Hello!!\n"
+    # In place of the line below, call a method to achieve the expected output.
+    actual = greeting.chomp
+    expected = "Hello!!"
+
+    assert_equal expected, actual
+  end
+
+  def test_10
+    # skip
+    greeting = "Hello!!\n\n"
+    # In place of the line below, call a method to achieve the expected output.
+    actual = greeting.chomp
+    expected = "Hello!!\n"
+
+    assert_equal expected, actual
+  end
+
+  def test_11
+    # skip
+    rhyme = "eeny, meeny, miny, moe"
+    # In place of the line below, call a method to achieve the expected output.
+    actual = rhyme.delete('e')
+    expected = "ny, mny, miny, mo"
+
+    assert_equal expected, actual
+  end
+
+  def test_12
+    # skip
+    rhyme = "eeny, meeny, miny, moe"
+    # In place of the line below, call a method to achieve the expected output.
+    actual = rhyme.gsub(/[eio]/, "")
+    expected = "ny, mny, mny, m"
+
+    assert_equal expected, actual
+  end
+
+  def test_13
+    # skip
+    greeting = "Hello World!"
+    # In place of the line below, call a method to get the number of characters in the string
+    actual = greeting.length
+    expected = 12
+
+    assert_equal expected, actual
+  end
+
+  def test_14
+    # skip
+    greeting = "Hello World!\n"
+    # In place of the line below, call a method to get the number of characters in the string
+    actual = greeting.length
+    expected = 13
+
+    assert_equal expected, actual
+  end
+
+  def test_15
+    # skip
+    greeting = "Hello       World!"
+    # In place of the line below, call a method to get the number of characters in the string
+    actual = greeting.length
+    expected = 18
+
+    assert_equal expected, actual
+  end
+
+  def test_16
+    # skip
+    greeting = "Hello World!"
+    # In place of the line below, call a method to get the number of 'o' in the string
+    actual = greeting.count('o')
+    expected = 2
+
+    assert_equal expected, actual
+  end
+
+  def test_17
+    # skip
+    greeting = "Hello World!"
+    # In place of the line below, call a method to get the number of vowels in the string
+    actual = greeting.count('aeiou')
+    expected = 3
+
+    assert_equal expected, actual
+  end
+
+  def test_18
+    # skip
+    greeting = "Hello World!"
+    # In place of the line below, call a method to check if the string includes 'llo'
+    actual = greeting.include?('llo')
+    expected = true
+
+    assert_equal expected, actual
+  end
+
+  def test_19
+    # skip
+    greeting = "Hello World!"
+    # In place of the line below, call a method to check if the string includes 'lol'
+    actual = greeting.include?('lol')
+    expected = false
+
+    assert_equal expected, actual
+  end
+
+  def test_20
+    # skip
+    greeting = "Hello World, my name is"
+    name = "Harry Potter"
+    # In place of the line below, use string manipulation to combine the
+    #greeting and name variables to acheive the expected outcome
+    actual = greeting.concat(name)
+    expected = "Hello World, my name is Harry Potter"
+  end
+  
+  def test_21
+    # skip
+    # See if you can use another method than the last test to achieve the same goal:
+    greeting = "Hello World, my name is"
+    name = "Harry Potter"
+    actual = greeting + " " + name
+    expected = "Hello World, my name is Harry Potter"
+
+    assert_equal expected, actual
+  end
+  
+  def test_22
+    # skip
+    greeting = "Hello World, my name is"
+    name = "Harry Potter"
+    # Again, using a different method:
+    actual = greeting.concat(" " + name)
+    expected = "Hello World, my name is Harry Potter"
+
+    assert_equal expected, actual
+  end
+  
+  def test_23
+    # skip
+    greeting = "Hello World, my name is"
+    name = "Harry Potter"
+    # Once more, using a different method:
+    actual = greeting.concat(" ") + name
+    expected = "Hello World, my name is Harry Potter"
+
+    assert_equal expected, actual
+  end
+
+  def test_24
+    # skip
+    phrase = "  \n\t to the moon\n\n\t    "
+    # In place of the line below, call a method to acheive the expected outcome
+    actual = phrase.strip
+    expected = "to the moon"
+
+    assert_equal expected, actual
   end
 end
